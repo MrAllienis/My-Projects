f7a8393fbcfd47cee021e6440ef72b7656e60dca
Andrei <151627787+MrAllienis@users.noreply.github.com>
2024-10-25 01:46:39 +0300

    Update settings.py

--- djangoprojects/4 admitad/sales/sales/settings.py
+++ djangoprojects/4 admitad/sales/sales/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r)urhv^xh9@=z8l4c1*ylnc!qg5l4su52*t7pj-9aw1iwptlzy'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
